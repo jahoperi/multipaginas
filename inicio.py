ff82f9f0ce521a3623a66ea0d89562a637de8ffe
jahoperi <97061564+jahoperi@users.noreply.github.com>
2023-06-01 09:12:20 -0600

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -1,5 +1,5 @@
 import streamlit as stm
   
-stm.set_page_config(page_title = "This is a Multipage WebApp")
+stm.set_page_config(page_title = "inicio")
 stm.title("This is the Home Page Geeks.")
 stm.sidebar.success("Select Any Page from here")
