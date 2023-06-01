fcb0915ea2f5b0422fcd64fbd4014e88bca7a326
jahoperi <97061564+jahoperi@users.noreply.github.com>
2023-06-01 09:23:34 -0600

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -7,4 +7,15 @@ stm.markdown("<h1 style='text-align: center; color: black;'>Centros de Atención
 
 stm.markdown("<h2 style='text-align: center; color: black;'>CAP's</h2>", unsafe_allow_html=True)
 
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.write(' ')
+
+with col2:
+    st.image("afore.jpeg")
+
+with col3:
+    st.write(' ')
+
 stm.sidebar.success("Seleccione cualquier página desde aquí")
