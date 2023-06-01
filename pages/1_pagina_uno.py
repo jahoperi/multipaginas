1a6e5604a809598520ab7ffa968112c39b9358ef
jahoperi <97061564+jahoperi@users.noreply.github.com>
2023-06-01 09:55:07 -0600

    Update 1_pagina_uno.py

--- pages/1_pagina_uno.py
+++ pages/1_pagina_uno.py
@@ -2,7 +2,7 @@ import streamlit as stm
 
 #stm.title("Página 1 de la presentación")
 
-stm.markdown("<h1 style='text-align: center; color: black;'>LLevar a cabo un análisis que nos permita ubicar los CAP´s que deben abrirse a lo largo de la República Mexicana, y cuales tal vez no son tan rentables</h1>", unsafe_allow_html=True)
+stm.markdown("<h1 style='text-align: justify; color: black;'>LLevar a cabo un análisis que nos permita ubicar los CAP´s que deben abrirse a lo largo de la República Mexicana, y cuales tal vez no son tan rentables</h1>", unsafe_allow_html=True)
 
 
 
