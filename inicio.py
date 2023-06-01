02bcce738248c0612ab048a5c9261722b7e8f59d
jahoperi <97061564+jahoperi@users.noreply.github.com>
2023-06-01 08:57:35 -0600

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -1,5 +1,5 @@
 import streamlit as stm
   
 stm.set_page_config(page_title = "Presentación")
-stm.title("CAP's Centros de atención al público ")
+stm.title("CAP's        Centros de atención al público ")
 stm.sidebar.success("Seleccionar cualquier página desde aquí")
