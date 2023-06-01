f6182d63067d9215180b52b1ab809532998081b0
jahoperi <97061564+jahoperi@users.noreply.github.com>
2023-06-01 08:34:48 -0600

    Update 1_pagina_uno.py

--- pages/1_pagina_uno.py
+++ pages/1_pagina_uno.py
@@ -1,4 +1,33 @@
 import streamlit as stm
+import pandas as pd
+import plotly.express as px
   
 stm.title("Página 1 de la presentación")
 stm.sidebar.success("Actualmente visualizas la página 1")
+
+st.header("Fruit List")
+
+_dic = {
+'Name: ['Mango', 'Apple', 'Banana'],
+'Quantity': [45, 38, 90]
+}
+
+_df = pd.Dataframe(_dic)
+
+load = st.button('Load Data')
+
+
+if "load_state" not in st.session_state:
+    st.session_state.load_state = False
+
+if load or st.session_state.load_state:
+    st.session_state.load_state = True
+    st.write(_df)
+
+    opt = st.radio('Plot type :', ['Bar', 'Pie'])
+    if opt == 'Bar':
+           fig = px.Bar(_df, x = 'Name', y = 'Quantity', title = 'Bar Chart')
+           st.plotly_chart(fig)
+    else:
+           fig = px.pie(_df, x = 'Name', y = 'Quantity', title = 'Pie Chart')
+           st.plotly_chart(fig)
