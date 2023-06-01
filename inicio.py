3b90b13a4c69d57157813ff8562e2bab365d7909
jahoperi <97061564+jahoperi@users.noreply.github.com>
2023-06-01 09:19:12 -0600

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -1,10 +1,10 @@
 import streamlit as stm
   
 stm.set_page_config(page_title = "inicio")
-stm.title("Centros de Atención al Público")
+#stm.title("Centros de Atención al Público")
 
-stm.markdown("<h1 style='text-align: center; color: grey;'>CAP's</h1>", unsafe_allow_html=True)
+stm.markdown("<h1 style='text-align: center; color: black;'>Centros de Atención al Público</h1>", unsafe_allow_html=True)
 
-#stm.markdown("<h2 style='text-align: center; color: black;'>Smaller headline in black </h2>", unsafe_allow_html=True)
+stm.markdown("<h2 style='text-align: center; color: black;'>CAP's</h2>", unsafe_allow_html=True)
 
 stm.sidebar.success("Seleccione cualquier página desde aquí")
