9e3c31fc9dd52b9d54b8616053729ce57e56c966
jahoperi <97061564+jahoperi@users.noreply.github.com>
2023-06-01 09:14:58 -0600

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -2,4 +2,9 @@ import streamlit as stm
   
 stm.set_page_config(page_title = "inicio")
 stm.title("Centros de Atención al Público")
+
+stm.markdown("<h1 style='text-align: center; color: grey;'>Big headline</h1>", unsafe_allow_html=True)
+
+stm.markdown("<h2 style='text-align: center; color: black;'>Smaller headline in black </h2>", unsafe_allow_html=True)
+
 stm.sidebar.success("Seleccione cualquier página desde aquí")
