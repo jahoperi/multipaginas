b7691c37b62af58a148d41c228baa9870d83a532
jahoperi <97061564+jahoperi@users.noreply.github.com>
2023-06-01 08:07:32 -0600

    Add files via upload

---
+++ pages/1_pagina_uno.py
@@ -0,0 +1,4 @@
+import streamlit as stm
+  
+stm.title("This is PageOne Geeks.")
+stm.sidebar.success("You are currently viewing Page One Geek")
---
+++ pages/2_pagina_2.py
@@ -0,0 +1,4 @@
+import streamlit as stm
+  
+stm.title("This is PageTwo Geeks.")
+stm.sidebar.success("You are currently viewing Page Two Geek")
