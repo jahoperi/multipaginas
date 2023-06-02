eb6d3356d115a541531debb0af0e358e64757275
jahoperi <97061564+jahoperi@users.noreply.github.com>
2023-06-02 09:08:33 -0600

    Update 1_Proyecto.py

--- pages/1_Proyecto.py
+++ pages/1_Proyecto.py
@@ -2,7 +2,11 @@ import streamlit as stm
 
 #stm.title("Página 1 de la presentación")
 
-stm.markdown("<h1 style='text-align: justify; color: black;'>Estudios de ubicación de CAP’s. Introducción. En PENSIONISSSTE es necesario contar con excelentes instalaciones, así como tener el personal capacitado para dar un servicio con la mejor calidad. PENSIONISSSTE pretende realizar un estudio para asentar nuevos Centros de Atención al Público conocidos como CAP’s en el espacio geográfico de las 32 entidades federativas, lo que implicaría que en las ciudades más concurridas, toda persona pueda tener acceso a un CAP cerca de su domicilio o cerca del lugar donde labora; y poder así incorporar a más trabajadores a AFORE PENSIONISSSTE. De igual manera, este estudio permitirá a PENSIONISSSTE saber si un CAP es rentable en su ubicación actual para así poder añadir nuevo personal, servicios y/o tecnologías, en caso contrario, si un CAP no es rentable este debe cerrar.</h1>", unsafe_allow_html=True)
+stm.markdown("<h1 style='text-align: center; color: black;'>Estudios de ubicación de CAP’s</h1>", unsafe_allow_html=True)
+
+stm.markdown("<h1 style='text-align: justify; color: black;'>Introducción</h1>", unsafe_allow_html=True)
+
+stm.markdown("<h1 style='text-align: justify; color: black;'>En PENSIONISSSTE es necesario contar con excelentes instalaciones, así como tener el personal capacitado para dar un servicio con la mejor calidad.</h1>", unsafe_allow_html=True)
 
 
 
