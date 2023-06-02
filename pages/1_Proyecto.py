8a68ca2cadbf4d8dc3aeaa5d86fc1fb4b5f2cf46
jahoperi <97061564+jahoperi@users.noreply.github.com>
2023-06-02 09:48:26 -0600

    Update 1_Proyecto.py

--- pages/1_Proyecto.py
+++ pages/1_Proyecto.py
@@ -15,13 +15,9 @@ stm.markdown("")
 stm.markdown("")
 stm.markdown("")
 
-#stm.markdown("<h1 style='text-align: justify; color: black;'>"I.-Introducción</h1>", unsafe_allow_html=True)
 
 a = '<p style="font-family:Courier; color:Black; font-size: 30px;">En PENSIONISSSTE es necesario contar con excelentes instalaciones, así como tener el personal capacitado para dar un servicio con la mejor calidad.</p>'
 stm.markdown(a, unsafe_allow_html=True)
-
-
-#stm.markdown("<h1 style='text-align: justify; color: black;'>En PENSIONISSSTE es necesario contar con excelentes instalaciones, así como tener el personal capacitado para dar un servicio con la mejor calidad.</h1>", unsafe_allow_html=True)
 
 b = '<p style="font-family:Courier; color:Black; font-size: 30px;">PENSIONISSSTE pretende realizar un estudio para asentar nuevos Centros de Atención al Público conocidos como CAP’s en el espacio geográfico de las 32 entidades federativas, lo que implicaría que en las ciudades más concurridas, toda persona pueda tener acceso a un CAP cerca de su domicilio o cerca del lugar donde labora; y poder así incorporar a más trabajadores a AFORE PENSIONISSSTE.</p>'
 stm.markdown(b, unsafe_allow_html=True)
@@ -31,18 +27,18 @@ stm.markdown(b, unsafe_allow_html=True)
 c = '<p style="font-family:Courier; color:Black; font-size: 30px;">De igual manera, este estudio permitirá a PENSIONISSSTE saber si un CAP es rentable en su ubicación actual para así poder añadir nuevo personal, servicios y/o tecnologías, en caso contrario, si un CAP no es rentable este debe cerrar.</p>'
 stm.markdown(c, unsafe_allow_html=True)
 
-
-#stm.markdown("<h1 style='text-align: justify; color: black;'>De igual manera, este estudio permitirá a PENSIONISSSTE saber si un CAP es rentable en su ubicación actual para así poder añadir nuevo personal, servicios y/o tecnologías, en caso contrario, si un CAP no es rentable este debe cerrar.</h1>", unsafe_allow_html=True)
-
-#stm.markdown("<h1 style='text-align: justify; color: black;'>2.-Objetivo</h1>", unsafe_allow_html=True)
-
 stm.markdown("")
 stm.markdown("")
 stm.markdown("")
 
 original_objetivo = '<p style="font-family:Courier; color:Blue; font-size: 40px;">2.- Objetivo</p>'
 stm.markdown(original_objetivo, unsafe_allow_html=True)
-stm.markdown("<h1 style='text-align: justify; color: black;'>Realizar trabajo de investigación y aplicar técnicas estadísticas para el análisis de datos que permita tomar las mejores decisiones para ubicar, reubicar o cerrar los Centros de Atención al Público (CAP´s) de PENSIONISSSTE a lo largo del país.</h1>", unsafe_allow_html=True)
+
+d = '<p style="font-family:Courier; color:Black; font-size: 30px;">Realizar trabajo de investigación y aplicar técnicas estadísticas para el análisis de datos que permita tomar las mejores decisiones para ubicar, reubicar o cerrar los Centros de Atención al Público (CAP´s) de PENSIONISSSTE a lo largo del país.</p>'
+stm.markdown(d, unsafe_allow_html=True)
+
+
+#stm.markdown("<h1 style='text-align: justify; color: black;'>Realizar trabajo de investigación y aplicar técnicas estadísticas para el análisis de datos que permita tomar las mejores decisiones para ubicar, reubicar o cerrar los Centros de Atención al Público (CAP´s) de PENSIONISSSTE a lo largo del país.</h1>", unsafe_allow_html=True)
 
 stm.markdown("")
 stm.markdown("")
