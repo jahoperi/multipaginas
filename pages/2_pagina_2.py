c9e40beeb93f064f6f0a783c2c9521183507ca98
jahoperi <97061564+jahoperi@users.noreply.github.com>
2023-06-02 08:34:22 -0600

    Update 2_pagina_2.py

--- pages/2_pagina_2.py
+++ pages/2_pagina_2.py
@@ -9,7 +9,7 @@ stm.sidebar.success("Actualmente visualizas la página 2")
 stm.header("Fruit List")
 
 _dic = {
-'Name': ['Nuevo León', 'Chihuahua', 'Baja California', 'Ciudad de México', 'Sinaloa', 'Baja California Sur', 'México', 'Tamaulipas', 'Quintana Roo', 'Sonora'. 'Coahuila', 'Jalisco', 'Querétaro', 'Guanajuato', 'Hidalgo', 'Yucatán', 'Aguascalientes', 'Colima', 'Durango', 'San Luis Potosí', 'Michoacán', 'Veracruz', 'Guerrero', 'Oaxaca', 'Nayarit', 'Puebla', 'Morelos', 'Tlaxcala', 'Tabasco', 'Zacatecas', 'Campeche', 'Chiapas'],
+'Name': ['Nuevo León', 'Chihuahua', 'Baja California', 'Ciudad de México', 'Sinaloa', 'Baja California Sur', 'México', 'Tamaulipas', 'Quintana Roo', 'Sonora', 'Coahuila', 'Jalisco', 'Querétaro', 'Guanajuato', 'Hidalgo', 'Yucatán', 'Aguascalientes', 'Colima', 'Durango', 'San Luis Potosí', 'Michoacán', 'Veracruz', 'Guerrero', 'Oaxaca', 'Nayarit', 'Puebla', 'Morelos', 'Tlaxcala', 'Tabasco', 'Zacatecas', 'Campeche', 'Chiapas'],
 'Quantity': [99.0, 91.15, 90.6, 90.6, 85.4, 83.3, 77.1, 70.8, 70.3, 69.3, 64.6, 64.6, 63.0, 62.5,62.0, 60.9, 58.9, 55.2, 54.7, 53.6, 51.6, 51.6, 51.0, 49.5, 45.8, 43.8, 41.1, 40.1, 34.4, 33.3, 32.8, 22.4]
 }
 
