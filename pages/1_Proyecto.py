01e5706004ae6a641e824bc99e1cc1a37f5e098a
jahoperi <97061564+jahoperi@users.noreply.github.com>
2023-06-02 09:21:39 -0600

    Update 1_Proyecto.py

--- pages/1_Proyecto.py
+++ pages/1_Proyecto.py
@@ -4,24 +4,24 @@ import streamlit as stm
 
 stm.markdown("<h1 style='text-align: center; color: black;'>Estudios de ubicación de CAP’s</h1>", unsafe_allow_html=True)
 
-stm.markdown("<h1 style='text-align: justify; color: black;'>Introducción</h1>", unsafe_allow_html=True)
+stm.markdown("<h1 style='text-align: justify; color: black;'>I.-Introducción</h1>", unsafe_allow_html=True)
 
 stm.markdown("<h1 style='text-align: justify; color: black;'>En PENSIONISSSTE es necesario contar con excelentes instalaciones, así como tener el personal capacitado para dar un servicio con la mejor calidad.</h1>", unsafe_allow_html=True)
 stm.markdown("<h1 style='text-align: justify; color: black;'>PENSIONISSSTE pretende realizar un estudio para asentar nuevos Centros de Atención al Público conocidos como CAP’s en el espacio geográfico de las 32 entidades federativas, lo que implicaría que en las ciudades más concurridas, toda persona pueda tener acceso a un CAP cerca de su domicilio o cerca del lugar donde labora; y poder así incorporar a más trabajadores a AFORE PENSIONISSSTE.</h1>", unsafe_allow_html=True)
 stm.markdown("<h1 style='text-align: justify; color: black;'>De igual manera, este estudio permitirá a PENSIONISSSTE saber si un CAP es rentable en su ubicación actual para así poder añadir nuevo personal, servicios y/o tecnologías, en caso contrario, si un CAP no es rentable este debe cerrar.</h1>", unsafe_allow_html=True)
 
-stm.markdown("<h1 style='text-align: justify; color: black;'>Objetivo</h1>", unsafe_allow_html=True)
+stm.markdown("<h1 style='text-align: justify; color: black;'>2.-Objetivo</h1>", unsafe_allow_html=True)
 stm.markdown("<h1 style='text-align: justify; color: black;'>Realizar trabajo de investigación y aplicar técnicas estadísticas para el análisis de datos que permita tomar las mejores decisiones para ubicar, reubicar o cerrar los Centros de Atención al Público (CAP´s) de PENSIONISSSTE a lo largo del país.</h1>", unsafe_allow_html=True)
-stm.markdown("<h1 style='text-align: justify; color: black;'>Requerimientos</h1>", unsafe_allow_html=True)
-stm.markdown("<h1 style='text-align: justify; color: black;'>Localización de cada CAP con latitud y longitud</h1>", unsafe_allow_html=True)
-stm.markdown("<h1 style='text-align: justify; color: black;'>Número de personas atendidas de cada CAP por semana o en su caso por mes</h1>", unsafe_allow_html=True)
-stm.markdown("<h1 style='text-align: justify; color: black;'>Número de personas que trabajan en cada CAP</h1>", unsafe_allow_html=True)
-stm.markdown("<h1 style='text-align: justify; color: black;'>Número de habitantes por municipio</h1>", unsafe_allow_html=True)
-stm.markdown("<h1 style='text-align: justify; color: black;'>Ubicar los CAP’s de la competencia</h1>", unsafe_allow_html=True)
-stm.markdown("<h1 style='text-align: justify; color: black;'>Número de personas en el municipio con una CI</h1>", unsafe_allow_html=True)
-stm.markdown("<h1 style='text-align: justify; color: black;'>Número de personas en el municipio afiliados a AFORE PENSIONISSSTE</h1>", unsafe_allow_html=True)
-stm.markdown("<h1 style='text-align: justify; color: black;'>Número de traspasos por entidad</h1>", unsafe_allow_html=True)
-stm.markdown("<h1 style='text-align: justify; color: black;'>Monto de traspasos</h1>", unsafe_allow_html=True)
+stm.markdown("<h1 style='text-align: justify; color: black;'>3.-Requerimientos</h1>", unsafe_allow_html=True)
+stm.markdown("<h1 style='text-align: justify; color: black;'>a) Localización de cada CAP con latitud y longitud</h1>", unsafe_allow_html=True)
+stm.markdown("<h1 style='text-align: justify; color: black;'>b) Número de personas atendidas de cada CAP por semana o en su caso por mes</h1>", unsafe_allow_html=True)
+stm.markdown("<h1 style='text-align: justify; color: black;'>c) Número de personas que trabajan en cada CAP</h1>", unsafe_allow_html=True)
+stm.markdown("<h1 style='text-align: justify; color: black;'>d) Número de habitantes por municipio</h1>", unsafe_allow_html=True)
+stm.markdown("<h1 style='text-align: justify; color: black;'>e) Ubicar los CAP’s de la competencia</h1>", unsafe_allow_html=True)
+stm.markdown("<h1 style='text-align: justify; color: black;'>f) Número de personas en el municipio con una CI</h1>", unsafe_allow_html=True)
+stm.markdown("<h1 style='text-align: justify; color: black;'>g) Número de personas en el municipio afiliados a AFORE PENSIONISSSTE</h1>", unsafe_allow_html=True)
+stm.markdown("<h1 style='text-align: justify; color: black;'>h) Número de traspasos por entidad</h1>", unsafe_allow_html=True)
+stm.markdown("<h1 style='text-align: justify; color: black;'>i) Monto de traspasos</h1>", unsafe_allow_html=True)
 
 
 stm.sidebar.success("La página se visualiza correctamente")
