9654fbab13483d1cde659fe70d4e392c7acfe9bb
jahoperi <97061564+jahoperi@users.noreply.github.com>
2023-06-02 09:24:57 -0600

    Update 1_Proyecto.py

--- pages/1_Proyecto.py
+++ pages/1_Proyecto.py
@@ -4,7 +4,7 @@ import streamlit as stm
 
 stm.markdown("<h1 style='text-align: center; color: black;'>Estudios de ubicación de CAP’s</h1>", unsafe_allow_html=True)
 
-stm.markdown("<h1 style='text-align: justify; color: black;'>red[I.-Introducción]</h1>", unsafe_allow_html=True)
+stm.markdown("<h1 style='text-align: justify; color: black;'>:red[I.-Introducción]</h1>", unsafe_allow_html=True)
 
 stm.markdown("<h1 style='text-align: justify; color: black;'>En PENSIONISSSTE es necesario contar con excelentes instalaciones, así como tener el personal capacitado para dar un servicio con la mejor calidad.</h1>", unsafe_allow_html=True)
 stm.markdown("<h1 style='text-align: justify; color: black;'>PENSIONISSSTE pretende realizar un estudio para asentar nuevos Centros de Atención al Público conocidos como CAP’s en el espacio geográfico de las 32 entidades federativas, lo que implicaría que en las ciudades más concurridas, toda persona pueda tener acceso a un CAP cerca de su domicilio o cerca del lugar donde labora; y poder así incorporar a más trabajadores a AFORE PENSIONISSSTE.</h1>", unsafe_allow_html=True)
