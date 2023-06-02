de7c85a3fd24db9974a79f63df80e7b20a41227c
jahoperi <97061564+jahoperi@users.noreply.github.com>
2023-06-02 10:46:58 -0600

    Update 1_Proyecto.py

--- pages/1_Proyecto.py
+++ pages/1_Proyecto.py
@@ -22,8 +22,8 @@ stm.markdown(a, unsafe_allow_html=True)
 b = '<p style="text-align: justify; font-family:Courier; color:Black; font-size: 30px;">PENSIONISSSTE pretende realizar un estudio para asentar nuevos Centros de Atención al Público conocidos como CAP’s en el espacio geográfico de las 32 entidades federativas, lo que implicaría que en las ciudades más concurridas, todo trabajador pueda tener acceso a un CAP cerca de su domicilio o del lugar donde labora; y poder así incorporar a más trabajadores a AFORE PENSIONISSSTE.</p>'
 stm.markdown(b, unsafe_allow_html=True)
 
-#c = '<p style="text-align: justify, font-family:Courier; color:Black; font-size: 30px;">De igual manera, este estudio permitirá a PENSIONISSSTE saber si un CAP es rentable en su ubicación actual para así poder añadir nuevo personal, servicios y/o tecnologías, en caso contrario, si un CAP no es rentable este debe cerrar.</p>'
-#stm.markdown(c, unsafe_allow_html=True)
+c = '<p style="text-align: justify; font-family:Courier; color:Black; font-size: 30px;">De igual manera, este estudio permitirá a PENSIONISSSTE saber si un CAP es rentable en su ubicación actual para así poder añadir nuevo personal, servicios y/o tecnologías, en caso contrario, si un CAP no es rentable este debe cerrar.</p>'
+stm.markdown(c, unsafe_allow_html=True)
 
 stm.markdown("")
 stm.markdown("")
