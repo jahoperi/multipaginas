3fe04b0dbd41127d366cbfcd38d6e0de4c835fa5
jahoperi <97061564+jahoperi@users.noreply.github.com>
2023-06-02 09:41:17 -0600

    Update 1_Proyecto.py

--- pages/1_Proyecto.py
+++ pages/1_Proyecto.py
@@ -17,6 +17,10 @@ stm.markdown("")
 
 #stm.markdown("<h1 style='text-align: justify; color: black;'>"I.-Introducción</h1>", unsafe_allow_html=True)
 
+a = '<p style="font-family:Courier; color:Black; font-size: 25px;">En PENSIONISSSTE es necesario contar con excelentes instalaciones, así como tener el personal capacitado para dar un servicio con la mejor calidad.</p>'
+stm.markdown(a, unsafe_allow_html=True)
+
+
 stm.markdown("<h1 style='text-align: justify; color: black;'>En PENSIONISSSTE es necesario contar con excelentes instalaciones, así como tener el personal capacitado para dar un servicio con la mejor calidad.</h1>", unsafe_allow_html=True)
 stm.markdown("<h1 style='text-align: justify; color: black;'>PENSIONISSSTE pretende realizar un estudio para asentar nuevos Centros de Atención al Público conocidos como CAP’s en el espacio geográfico de las 32 entidades federativas, lo que implicaría que en las ciudades más concurridas, toda persona pueda tener acceso a un CAP cerca de su domicilio o cerca del lugar donde labora; y poder así incorporar a más trabajadores a AFORE PENSIONISSSTE.</h1>", unsafe_allow_html=True)
 stm.markdown("<h1 style='text-align: justify; color: black;'>De igual manera, este estudio permitirá a PENSIONISSSTE saber si un CAP es rentable en su ubicación actual para así poder añadir nuevo personal, servicios y/o tecnologías, en caso contrario, si un CAP no es rentable este debe cerrar.</h1>", unsafe_allow_html=True)
